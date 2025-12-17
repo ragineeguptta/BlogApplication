d7d3a5a8b571734bcf7cb6457888ae7cb47838c8
Raginee Gupta <ragineegupta123@gmail.com>
2025-12-17 16:31:47 +0530

    created list add category view and routing

--- UI/BlogApp/src/app/app.routes.ts
+++ UI/BlogApp/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CategoryList } from './feature/category/category-list/category-list';
+import { AddCategory } from './feature/category/add-category/add-category';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'admin/category',
+        component: CategoryList
+    },
+    {
+        path: 'admin/category/add',
+        component: AddCategory
+    }
+];
---
+++ UI/BlogApp/src/app/feature/category/add-category/add-category.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+
+@Component({
+  selector: 'app-add-category',
+  imports: [ReactiveFormsModule],
+  templateUrl: './add-category.html',
+  styleUrl: './add-category.css',
+})
+export class AddCategory {
+  addCategoryFormGroup = new FormGroup({
+    name: new FormControl<string>('', {
+      nonNullable: true, 
+      validators : [Validators.required, Validators.maxLength(100)]
+    }),
+    urlHandle: new FormControl<string>('', {
+      nonNullable: true,
+      validators : [Validators.required, Validators.maxLength(200)]
+    }),
+  });
+
+  get nameFormControl(){
+    return this.addCategoryFormGroup.controls.name;
+  }
+  get urlHandleFormControl(){
+    return this.addCategoryFormGroup.controls.urlHandle;
+  }
+
+onSubmit(){
+  console.log(this.addCategoryFormGroup.getRawValue());
+}
+
+}
