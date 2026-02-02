46bea317e864fe631c9b9486d4a802fa6a9a229d
Raginee Gupta <ragineegupta123@gmail.com>
2026-02-02 23:13:27 +0530

    blogpost

---
+++ UI/BlogApp/src/app/feature/blogpost/blogpost-list/blogpost-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blogpost-list',
+  imports: [],
+  templateUrl: './blogpost-list.html',
+  styleUrl: './blogpost-list.css',
+})
+export class BlogpostList {
+
+}
